5b73740b12711f3aab2ccd0c2d83c25ce4abd802
David Kopriva <kopriva@math.fsu.edu>
2021-05-03 11:10:07 -0700

    Update version number.

--- Source/HOHQMeshMain.f90
+++ Source/HOHQMeshMain.f90
@@ -43,7 +43,7 @@
 !        Other
 !        -----
 !         
-         CHARACTER(LEN=8) :: version           = "8.21.20"
+         CHARACTER(LEN=8) :: version           = "05.03.21"
          LOGICAL          :: debug             = .FALSE.
          LOGICAL          :: didGenerate3DMesh = .FALSE.
          LOGICAL          :: shouldGenerate3D  = .FALSE.
