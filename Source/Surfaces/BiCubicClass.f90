170028d953c41f530ab45100f13aa836919f5af1
David Kopriva <kopriva@math.fsu.edu>
2022-06-07 09:00:08 -0700

    Code Cleanup
    Remove unused variables. Ensure input files are closed before moving to another benchmark. Removed bottomFromFile test to see if that is where the problem is coming from.

--- Source/Surfaces/BiCubicClass.f90
+++ Source/Surfaces/BiCubicClass.f90
@@ -228,7 +228,7 @@
          ! local variables
          INTEGER       :: i, j, k
          REAL(KIND=RP) :: u, v, z_corners(4), dz_dx(4), dz_dy(4), dz_dxy(4)
-         REAL(KIND=RP) :: dx, dy, temp
+         REAL(KIND=RP) :: dx, dy
          REAL(KIND=RP), DIMENSION(16) :: work_vec, coeff_vec
          REAL(KIND=RP), DIMENSION(4,4) :: coeffs
 
@@ -452,7 +452,7 @@
          IMPLICIT NONE  
          
          TYPE(BiCubicInterpolation) :: bQ
-         REAL(KIND=RP)              :: z, x, y
+         REAL(KIND=RP)              :: x, y
          INTEGER                    :: k
          LOGICAL                    :: tst
          
