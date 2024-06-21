b924a2d9a101f447010c8dce1408eb1524eb9053
Andrew Winters <andrew.ross.winters@liu.se>
2024-06-21 21:43:55 +0200

    Increment version to v1.5.1

--- Source/HOHQMeshMain.f90
+++ Source/HOHQMeshMain.f90
@@ -62,7 +62,7 @@
 !        Other
 !        -----
 !
-         CHARACTER(LEN=*), PARAMETER :: version           = "1.5.1-pre"
+         CHARACTER(LEN=*), PARAMETER :: version           = "1.5.1"
          LOGICAL                     :: test              = .FALSE.
          LOGICAL                     :: generateTest      = .FALSE.
          LOGICAL                     :: didGenerate3DMesh = .FALSE.
